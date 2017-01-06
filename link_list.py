0338bc58f0f19a5b84fe6a8e867d9fe5bea0b26c
regenalgrant <regenal@mac.com>
2017-01-05 17:58:14 -0800

    built pop size and search methods and all tests pass

--- link_list.py
+++ link_list.py
@@ -27,3 +27,31 @@ class Link_List(object):
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def pop(self):
+        """Pop head from linklist."""
+        pop_old_node = self.head
+        try:
+            self.head = self.head.next_node
+        except AttributeError:
+            raise AttributeError("Cannot pop from empty list")
+        return pop_old_node.data
+
+    def size(self):
+        """Size method finds length of linklist."""
+        counter = 0
+        current = self.head
+        while current is not None:
+            counter += 1
+            current = current.next_node
+        return counter
+
+    def search(self, data):
+        """Searching for Node with a value."""
+        current = self.head
+        try:
+            while data != current.data:
+                current = current.next_node
+            return current
+        except AttributeError:
+            raise IndexError("Did not find value")
--- test_link_list.py
+++ test_link_list.py
@@ -1,20 +1,60 @@
+from __future__ import unicode_literals
 import pytest 
-from link_list import Link_List
+from link_list import Link_List, Node
 
-# TEST_INIT_DATA = [1, 2, 3, 4]
 
-# @pytest.mark.paramatrize("data", TEST_INIT_DATA)
 def test_init():
     """testing for init method of linklist."""
     test_instance = Link_List([1, 2, 3, 4])
     assert test_instance.head.data == 4
 
+
 def test_push():
     """Testing push node to the link list."""
     test_instance = Link_List()
-    # test_instance.push(10)
-    # assert test_instance.head.data == 10
+    test_instance.push(10)
+    assert test_instance.head.data == 10
     assert test_instance.head is not None
+
+
+def test_pop():
+    """Testing pop node off the head."""
+    test_instance = Link_List("newhead")
+    assert test_instance.pop() == "d"
+
+
+def test_pop_empty_list():
+    """Test for error message when popping empty list."""
+    test_instance = Link_List()
+    with pytest.raises(AttributeError) as message:
+        test_instance.pop()
+    assert "Cannot pop from empty list" in str(message)
+
+
+def test_size():
+    """Testing size method for linklist."""
+    test_instance = Link_List([1, 2, 3, 4])
+    assert test_instance.size() == 4
+
+
+def test_search_value_not_list():
+    """"Testing for value."""
+    test_instance = Link_List("NotHere")
+    test_node = Node("notnode")
+    with pytest.raises(IndexError) as message:
+            test_instance.search(test_node.data)
+    assert "Did not find value" in str(message)
+
+
+def test_search_value():
+    """Searching a value in a Node."""
+    test_instance = Link_List("IamHere")
+    test_node = Node("I")
+    assert test_instance.search(test_node.data).data == "I"
+
+
+
+
 
 
 
