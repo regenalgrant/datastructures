4e0aa148bb014226987b4ba960344c8829133fea
regenalgrant <regenal@mac.com>
2017-01-19 18:08:01 -0800

    added Testing for push method mutiple time.

--- src/test_dll.py
+++ src/test_dll.py
@@ -16,28 +16,42 @@ def test_init_check_tail():
 def test_init_not_iterable():
     """Testing for non-iterable data."""
     with pytest.raises(IndexError) as message:
-        test_instance = DoublyLinkedList("l")
+        test_instance = DoublyLinkedList(7)
     assert "Need to pass an iterable value" in str(message)
 
-def test_push_method():
+def test_push_existing_list():
     """Testing for nodes are being added the head of list."""
     test_instance = DoublyLinkedList("word")
     test_instance.push("two")
     assert test_instance.head.data == "two"
 
-def test_push_return():
-
-def test_pop_method():
-
-def test_pop_retun():
-
-def test_pop_empty():
-
 def test_push_empty():
+    """Testing is push node to a empty list."""
+    test_instance = DoublyLinkedList()
+    test_instance.push(1)
+    assert test_instance.head.data == test_instance.tail.data
 
-def test_append_method():
-def test_append_to_tail():
+def test_push_mutiple_nodes():
+    """Testing for push method mutiple time."""
+    test_instance = DoublyLinkedList()
+    test_instance.push(4)
+    test_instance.push(5)
+    test_instance.push(6)
+    test_instance.push(7)
+    assert test_instance.head.next_node.data == test_instance.tail.previous.previous.data
 
-def test_shift_method():
 
-def test_remove_method():
+
+# def test_pop():
+#
+# def test_pop_retun():
+#
+# def test_pop_empty():
+
+#
+# def test_append_method():
+# def test_append_to_tail():
+#
+# def test_shift_method():
+#
+# def test_remove_method():
