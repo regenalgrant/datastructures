38de4fda1383852bbf575c19755528e13636c328
regenalgrant <regenal@mac.com>
2017-01-31 12:55:54 -0800

    save

---
+++ src/deque.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from dll import DoublyLinkedList
+
+
+class Deque(object):
+    """Creating template for deque."""
+
+    def __init__(self, iterable=None):
+        """Creating instances for the Deque."""
+        self.dll = DoublyLinkedList(iterable)
+
+    def append(self, data):
+        """Appending the value at the tail of the deque."""
+        self.dll.tail.data
+
+    def appendleft(self, data):
+        """Adds a value to the front of the deque."""
+        self.dll.tail.next_node
+
+    # def pop():
+    #     """Removes a value from the tail of the deque and returns."""
+    #
+    # def pop_left():
+    #     """removes a value from the front of the deque and returns."""
+
+    def peek(self):
+        """Return the value of the tail node."""
+        if self.dll.tail is None:
+            return
+        return self.dll.tail.data
+
+    # def peek_left():
+    #     Next value that would be returned by popleft
+
+    def size(self):
+        """Size of the deque returning zero."""
+        count = 0
+        current = self.dll.head
+        while current is not None:
+            count += 1
+            current = current.next_node
+        return count
---
+++ src/test_deque.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest
+from deque import Deque
+
+
+#---------------------peek---------------------------
+def test_peek_on_deque():
+    """Test peek on deque."""
+    test_deque = Deque([1, 2, 3, 4])
+    peek_tail_data = test_deque.peek()
+    assert peek_tail_data == test_deque.dll.tail.data
+
+def test_peek_on_empty_deque():
+    """Testing peek on an empty deque."""
+    test_deque = Deque()
+    peek_tail = test_deque.peek()
+    assert peek_tail is None
+# ------------------size------------------------------
+def test_length_of_empty_deque():
+    """Check that length of empty deque returns 0."""
+    test_deque = Deque()
+    assert test_deque.size() == 0
+
+def test_deque_size():
+    """Test that list initialized with data."""
+    test_deque = Deque([1, 2, 3, 4])
+    assert test_deque.size() == 4
+#-------------------append----------------------------
+def test_append_value_to_tail():
+    """Testing append a value to the tail of the deque."""
+    test_deque = Deque([1, 2, 3, 4])
+    assert test_deque.append() == 5
