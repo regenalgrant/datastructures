64cbd12ac567e44d51f811a6db64c41c192e0508
regenalgrant <regenal@mac.com>
2017-02-21 12:33:51 -0800

    test insert many, empty tree, and contains false.

--- src/test_bst.py
+++ src/test_bst.py
@@ -20,6 +20,31 @@ def instance():
     return root
 
 
-    def test_new_root(instance):
-        """Test that our root is a root."""
+    def test_new_tree(instance):
+        """Test that our tree is a tree."""
         assert isinstance(instance, BST)
+
+
+    def test_new_empty_tree(empty_instance):
+        """Test that an empty tree is still a tree."""
+        assert all
+        ([
+            test.empty_instance.date is None,
+            test.empty_instance.root is None,
+            test.empty_instance.left is None,
+            test.empty_instance.right is None
+        ])
+
+
+    def test_insert_many(empty_instance):
+        """Test insert method with many values."""
+        insertions = list(range(10))
+        for root in insertions:
+            test.empty_instance.insert(root)
+        for root in insertions:
+            assert test.empty_instance.contains(root)
+
+
+    def test_contains_false(instance):
+        """Test false example for contains method."""
+        assert not instance.contains(1000)
