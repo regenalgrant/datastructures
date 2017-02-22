782c9533104cf83dba798d293edc8584c2a59969
regenalgrant <regenal@mac.com>
2017-02-22 15:46:54 -0800

    add insert

--- src/priorityq.py
+++ src/priorityq.py
@@ -8,3 +8,14 @@ class PriorityQueue:
     def __init__(self):
         """Priority Queue constructor method."""
         self.dict = {}
+
+
+    def insert(self, data, priority):
+        """Priority Queue insert method."""
+        if not isinstance(priority, int):
+            raise TypeError("Priority must be an integer")
+        if priority in self.dict:
+            self.dict[priority].append(data)
+        else:
+            self.dict[priority] = [data]
+        print(self.dict)
