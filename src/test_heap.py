b0d302851a54aa2faf39c00cc634f9a30b56b9f9
regenalgrant <regenal@mac.com>
2017-02-22 22:14:15 -0800

    Test heap init

--- src/test_heap.py
+++ src/test_heap.py
@@ -1,1 +1,10 @@
+#  _*_coding:utf-8 _*_
+"""Test create a heap data structure."""
 
+data = [1, 2, 3, 4]
+
+
+def heap_init():
+    """Test heap init."""
+    from heap import Heap
+    assert isinstance(Heap() == Heap)
