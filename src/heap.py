b62fe016bef058f7617c167c8d954e0b89d3d5fc
regenalgrant <regenal@mac.com>
2017-02-22 22:07:10 -0800

    create init and class for heap

---
+++ src/heap.py
@@ -0,0 +1,12 @@
+#  _*_coding:utf-8 _*_
+"""Create a heap data structure."""
+from __future__ import division
+import math
+
+
+class Heap(object):
+    """Create a heap data structure."""
+
+    def __init__(self):
+        """Init method for Heap class."""
+        self.high_low = []
