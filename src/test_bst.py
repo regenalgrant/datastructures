0d45919f0343a1eb74717658d903f18489cd5483
regenalgrant <regenal@mac.com>
2017-02-22 16:50:19 -0800

    added test new and empty

--- src/test_bst.py
+++ src/test_bst.py
@@ -22,9 +22,23 @@ def instance():
 
 @pytest.fixture
 def instance2():
-    """Fun tree fixture."""
-    fun_tree = Bst()
+    """extra tree fixture."""
+    extra_tree = Bst()
     insertions = [7, 6, 10, 5, 20, 11]
-    for fun in insertions:
-        fun_tree.insert(fun)
-    return fun_tree
+    for extra in insertions:
+        extra_tree.insert(extra)
+    return extra_tree
+
+
+def test_new_tree(instance):
+    """Test that our tree is a tree."""
+    assert isinstance(instance, Bst)
+
+
+def test_new_empty_tree(empty_instance):
+    """Test that an empty tree is still a tree."""
+    assert all([
+        empty_instance.value is None,
+        empty_instance.parent is None,
+        empty_instance.left_child is None,
+        empty_instance.right_child is None])
