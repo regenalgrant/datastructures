1061058024fcd5b82d9a59b0973775da4620f7b1
regenalgrant <regenal@mac.com>
2017-02-22 20:46:28 -0800

    adding full graph and new graph

---
+++ src/test_graph.py
@@ -0,0 +1,28 @@
+# _*_ encoding: utf-8 _*_
+"""Test graph.py."""
+import pytest
+
+
+@pytest.fixture()
+def new_graph():
+    """Fixture for graph."""
+    from graph import Graph
+    test_graph = Graph()
+    test_graph.add_node("codefellows")
+    test_graph.add_node("python")
+    return test_graph
+
+
+@pytest.fixture()
+def full_graph():
+    """Fixture that makes a full graph."""
+    from graph import Graph
+    ample_graph = Graph()
+    ample_graph.add_edge("a", "b")
+    ample_graph.add_edge("c", "d")
+    ample_graph.add_edge("c", "b")
+    ample_graph.add_edge("a", "d")
+    ample_graph.add_edge("b", "d")
+    ample_graph.add_edge("d", "a")
+    print(ample_graph.graph)
+    return ample_graph
