867b842eca4132a36fe59b770c630a0875a2c889
regenalgrant <regenal@mac.com>
2017-02-23 02:59:46 -0800

    test string

--- src/test_radix.py
+++ src/test_radix.py
@@ -7,3 +7,10 @@ def test_radix_sort():
     from radix import radix_sort
     radix_return_val = [56, 93, 7, 11]
     assert radix_sort(radix_return_val) == [7, 11, 56, 93]
+
+
+def test_string_in_list():
+    """Assert error is raised input."""
+    from radix import radix_sort
+    with pytest.raises(TypeError):
+        radix_sort([1, 45, 'a', 5])
